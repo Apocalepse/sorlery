3e6f3cd94a91f88c7e26aef22d5bd6fbdfc2497c
lampslave <lampslave@gmail.com>
2016-07-10 10:10:50 +0300

    bring updates from sorl-thumbnail 12.4a1

--- sorlery/backend.py
+++ sorlery/backend.py
@@ -1,38 +1,61 @@
-from django.utils import six
+from __future__ import unicode_literals
+
+import logging
+from sorl.thumbnail.compat import string_type, text_type
 from sorl.thumbnail.conf import settings, defaults as default_settings
-from sorl.thumbnail.images import ImageFile
-from sorl.thumbnail.kvstores.base import add_prefix
 from sorl.thumbnail.helpers import serialize, deserialize
+from sorl.thumbnail.images import ImageFile, DummyImageFile
+from sorl.thumbnail.kvstores.base import add_prefix
 from sorl.thumbnail import default
 from sorl.thumbnail.base import ThumbnailBackend
 
 from sorlery.tasks import create_thumbnail
+
+logger = logging.getLogger(__name__)
 
 
 class QueuedThumbnailBackend(ThumbnailBackend):
     """
     Queue thumbnail generation with django-celery.
     """
+
     def get_thumbnail(self, file_, geometry_string, **options):
+        """
+        Returns thumbnail as an ImageFile instance for file with geometry and
+        options given. First it will try to get it from the key value store,
+        secondly it will create it.
+        """
+        logger.debug(text_type('Getting thumbnail for file [%s] at [%s]'),
+                     file_, geometry_string)
+
         async = options.pop('async', True)
-
         if not async:
-            return super(QueuedThumbnailBackend, self).get_thumbnail(file_, geometry_string, **options)
+            return super(QueuedThumbnailBackend, self).get_thumbnail(
+                file_, geometry_string, **options)
 
-        source = ImageFile(file_)
+        if file_:
+            source = ImageFile(file_)
+        elif settings.THUMBNAIL_DUMMY:
+            return DummyImageFile(geometry_string)
+        else:
+            return None
 
-        for key, value in self.default_options.iteritems():
+        # preserve image filetype
+        if settings.THUMBNAIL_PRESERVE_FORMAT:
+            options.setdefault('format', self._get_format(source))
+
+        for key, value in self.default_options.items():
             options.setdefault(key, value)
 
+        # For the future I think it is better to add options only if they
+        # differ from the default settings as below. This will ensure the same
+        # filenames being generated for new options at default.
         for key, attr in self.extra_options:
             value = getattr(settings, attr)
             if value != getattr(default_settings, attr):
                 options.setdefault(key, value)
 
-        # Generate a name for the thumbnail
         name = self._get_thumbnail_filename(source, geometry_string, options)
-
-        # See if we've got a hit in the cache
         thumbnail = ImageFile(name, default.storage)
         cached = default.kvstore.get(thumbnail)
         if cached:
@@ -45,7 +68,7 @@ class QueuedThumbnailBackend(ThumbnailBackend):
         # Note: If the thumbnail file has been deleted, you will need to manually
         # clear the corresponding row from the kvstore to have thumbnail rebuilt.
         job = create_thumbnail.delay(file_, geometry_string, options, name)
-        if isinstance(file_, six.string_types):
+        if isinstance(file_, string_type):
             filename = file_.split('/')[-1]
         else:
             filename = file_.name
@@ -69,7 +92,8 @@ class QueuedThumbnailBackend(ThumbnailBackend):
             rawvalue = default.kvstore._get_raw(add_prefix(thumbnail.key))
             rawvaluearr = deserialize(rawvalue)
             rawvaluearr['name'] = filename
-            default.kvstore._set_raw(add_prefix(thumbnail.key), serialize(rawvaluearr))
+            default.kvstore._set_raw(add_prefix(thumbnail.key),
+                                     serialize(rawvaluearr))
 
         thumbnail.name = filename
         return thumbnail
--- sorlery/tasks.py
+++ sorlery/tasks.py
@@ -1,32 +1,59 @@
+from __future__ import unicode_literals
+
+import logging
+
 from celery import shared_task
-from sorl.thumbnail import default
+from sorl.thumbnail.compat import text_type
+from sorl.thumbnail.conf import settings
 from sorl.thumbnail.images import ImageFile
+from sorl.thumbnail import default
+
+logger = logging.getLogger(__name__)
 
 
 @shared_task
 def create_thumbnail(file_, geometry_string, options, name, force=False):
-    thumbnail = ImageFile(name, default.storage)
-
-    if thumbnail.exists() and not force:
-        return
-
     if file_:
         source = ImageFile(file_)
     else:
         return
-    source_image = default.engine.get_image(source)
-    try:
-        image_info = default.engine.get_image_info(source_image)
-        options['image_info'] = image_info
-    except AttributeError:
-        options['image_info'] = {}
-    size = default.engine.get_image_size(source_image)
-    source.set_size(size)
 
-    default.backend._create_thumbnail(source_image, geometry_string, options, thumbnail)
-    if hasattr(default.backend, '_create_alternative_resolutions'):
-        default.backend._create_alternative_resolutions(source_image, geometry_string, options, thumbnail.name)
+    thumbnail = ImageFile(name, default.storage)
 
-    # Need to update both the source and the thumbnail with correct sizing
-    default.kvstore.set(source)
+    # We have to check exists() because the Storage backend does not
+    # overwrite in some implementations.
+    if settings.THUMBNAIL_FORCE_OVERWRITE or not thumbnail.exists() or force:
+        try:
+            source_image = default.engine.get_image(source)
+        except IOError as e:
+            logger.exception(e)
+            # if S3Storage says file doesn't exist remotely, don't try to
+            # create it and exit early.
+            # Will return working empty image type; 404'd image
+            logger.warn(
+                text_type('Remote file [%s] at [%s] does not exist'),
+                file_, geometry_string)
+            return
+
+        # We might as well set the size since we have the image in memory
+        try:
+            image_info = default.engine.get_image_info(source_image)
+            options['image_info'] = image_info
+        except AttributeError:
+            options['image_info'] = {}
+        size = default.engine.get_image_size(source_image)
+        source.set_size(size)
+
+        try:
+            default.backend._create_thumbnail(
+                source_image, geometry_string, options, thumbnail)
+            default.backend._create_alternative_resolutions(
+                source_image, geometry_string, options, thumbnail.name)
+        finally:
+            default.engine.cleanup(source_image)
+
+    # If the thumbnail exists we don't create it, the other option is
+    # to delete and write but this could lead to race conditions so I
+    # will just leave that out for now.
+    default.kvstore.get_or_set(source)
     default.kvstore.set(thumbnail, source)
